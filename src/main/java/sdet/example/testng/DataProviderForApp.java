91b6c512e3268a80f164e538f9b4f83877b7cec4
kaede <kaede-git-2013@example.local>
2013-08-09 06:51:01 -0700

    Added testNG @Factory

--- src/main/java/sdet/example/testng/DataProviderForApp.java
+++ src/main/java/sdet/example/testng/DataProviderForApp.java
@@ -24,5 +24,18 @@ public class DataProviderForApp {
 		return dataToBeReturned.iterator();	
 	}
 	
+	@DataProvider(name = DataProviderForApp.JUNE)
+	public static Iterator<Object[]>  dataForFactory() {
+		List<String> strList = Arrays.asList("2012","2013");
+		List<Object[]> dataToBeReturned = new ArrayList<Object[]>();
+		for (String str : strList) {
+			dataToBeReturned.add(new Object[] { str, "June" });
+		}
+		logger.debug("dataToBeReturned.size=" + dataToBeReturned.size());
+		return dataToBeReturned.iterator();	
+	}
+	
+	
 	final public static String FRIDAY = "YES_I_LIKE_EVERY_FRIDAY";
+	final public static String JUNE = "YES_I_LIKE_JUNE_BEGINNING_OF_SUMMER";
 }
