142ded8e8fbcd4b9bbc0645380fb707c8506242c
kaede <kaede-git-2013@example.local>
2016-02-14 10:33:36 -0800

    Added the priorities to run the test cases

--- src/test/java/local/example/testng/annotation/Sequencing.java
+++ src/test/java/local/example/testng/annotation/Sequencing.java
@@ -7,25 +7,24 @@ import org.testng.annotations.Test;
  */
 public class Sequencing {
 
-    @Test
+    @Test(priority = 2)
     public void testCase1() {
         System.out.println("This is the test case 1.");
     }
 
-    @Test
+    @Test(priority = 3)
     public void testCase2() {
         System.out.println("This is the test case 2.");
 
     }
 
-
-    @Test
+    @Test(priority = 0)
     public void testCase3() {
         System.out.println("This is the test case 3.");
 
     }
 
-    @Test
+    @Test(priority = 1)
     public void testCase4() {
         System.out.println("This is the test case 4.");
 
